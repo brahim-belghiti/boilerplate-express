653d1f566d35984fbd529b91ace93dcaaf3fc6f0
Brahim Belghiti <91473510+brahim-belghiti@users.noreply.github.com>
2023-12-15 18:11:38 +0100

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", function(req, res) {
-  res.send('Hello Express');
-}
-)
+// app.get("/", function(req, res) {
+//   res.send('Hello Express');
+// }
+// )
+const filePath = __dirname + "/views/index.html";
+app.get("/", function (req, res) {
+  res.sendFile(filePath);
+});
 
 
 
