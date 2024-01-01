7d12fee6e136f91b4e62033f0be209ac4e20a7f3
brahim-belghiti <brahim.belghiti@proton.me>
2024-01-01 16:03:22 +0100

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -10,21 +10,28 @@ app.get("/", (req, res) => {
   res.sendFile(indexPath);
 });
 
-app.get("/json", (req, res)=>{
+app.get("/json", (req, res) => {
   switch (process.env.MESSAGE_STYLE) {
     case "uppercase":
-    res.json({
-      message: "HELLO JSON"
-     })
-    break;
+      res.json({
+        message: "HELLO JSON"
+      })
+      break;
     default:
       res.json({
         message: "Hello json"
-       })
+      })
   }
-    
-  
+
+
 })
+
+app.get('/now', function (req, res, next) {
+  req.time = new Date().toString();  // Hypothetical synchronous operation
+  next();
+}, function (req, res) {
+  res.json({time:req.time});
+});
 
 // Serving static files from the 'public' directory
 app.use("/public", express.static(assetsPath));
@@ -65,4 +72,4 @@ module.exports = app;
 
 
 
- module.exports = app;
+module.exports = app;
